7d787963917c7f64b6f6a0356d5a1f6182394cdd
ACarmonaTS <ts.acarmona@outlook.com>
2023-05-19 15:44:48 -0600

    proyecto

---
+++ Capacitaciones.js
@@ -0,0 +1,27 @@
+// main.js
+
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    // Navbar
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const offcanvasNavbar = document.querySelector("#offcanvasNavbar");
+  
+    navbarToggler.addEventListener("click", function () {
+      offcanvasNavbar.classList.toggle("show");
+    });
+  
+    // Carrousel
+    const carousel = new bootstrap.Carousel(document.querySelector("#carouselExampleCaptions"), {
+      interval: false
+    });
+  
+    // Noticias
+    const noticiasCards = document.querySelectorAll(".noticias .card");
+  
+    noticiasCards.forEach(function (card) {
+      card.addEventListener("click", function () {
+        // Handle card click event
+      });
+    });
+  });
+  
